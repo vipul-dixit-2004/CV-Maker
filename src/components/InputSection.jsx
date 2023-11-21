2e268ea69aa98aeef22de5d2c0160534a44c6bac
Vipul Dixit <dixit.vipul0511@gmail.com>
2023-11-22 01:03:20 +0530

    Updated

--- src/components/InputSection.jsx
+++ src/components/InputSection.jsx
@@ -8,7 +8,8 @@ function InputSection(){
     const [emailInp,setEmail] = useState('')
     const [mobInp,setMob] = useState('')
     const [jobInp,setJob] = useState('')
-    // const [mobInp,setMob] = useState('')
+    const [jobPostInp,setJobPost] = useState('')
+    const [qualifiInp,setQualifi] = useState('')
     const [imgInp,setImg] = useState(profileLogo)
 return (
 <div className="mainApp">
@@ -16,6 +17,8 @@ return (
 <form>
     <label htmlFor="nameInp">Enter Name: </label>
     <input type="text" id="nameInp" name="nameInp" onChange={(e)=>setName(e.target.value)}/><br/>
+    <label htmlFor="nameInp">Applying for: </label>
+    <input type="text" id="postInp" name="postInp" onChange={(e)=>setJobPost(e.target.value)}/><br/>
     <label htmlFor="emailInp">Enter Email: </label>
     <input type="email" id="emailInp" name="emailInp" onChange={(e)=>setEmail(e.target.value)}/><br/>
     <label htmlFor="mobInp">Enter Contact No.: </label>
@@ -23,8 +26,8 @@ return (
     <label htmlFor="jobInt">Enter Job Details: </label>
     <textarea type="text" id="jobInp" name="jobInp" onChange={(e)=>setJob(e.target.value)}></textarea><br/>
     <label htmlFor="qualificationInp">Enter Qualifications: </label>
-    <textarea type="text" id="qualificationInp" name="qualificationInp"></textarea>,<br />
-    <label htmlFor="qualificationInp">Select image: </label>
+    <textarea type="text" id="qualificationInp" name="qualificationInp" onChange={(e)=>setQualifi(e.target.value)}></textarea>,<br />
+    <label htmlFor="imgInp">Select image: </label>
     <input type="file" name="imgInp" onChange={(e)=>setImg(e.target.value)}/>
 </form>
 </div>
@@ -32,7 +35,10 @@ return (
     <div className="CV">
         <div className="heading">
         <div className="personalInfo">
+            <div class="text-con">
             <h1>{getInput(nameInp,"Your Name")}</h1>
+            <h3>{getInput(jobPostInp,"(Post)")}</h3>
+            </div>
         </div>
         <div className="profile">
             <img src={imgInp} height={"200px"}  className="profileImage rounded float-end" alt="ProfilePhoto"/>
@@ -45,7 +51,13 @@ return (
         <div className="content">
             <div className="details">
                 <h2>Job Experiences</h2>
-                <pre>{getInput(jobInp,"")}</pre>
+                <div class="job-section">
+                    <pre>{getInput(jobInp,"Your job descriptions displays here......")}</pre>
+                </div>
+                <h2>Qualifications</h2>
+                <div className="qualification-section">
+                    <pre>{getInput(qualifiInp,"Your all Qualifications goes here.....")}</pre>
+                </div>
 
             </div>
         </div>
